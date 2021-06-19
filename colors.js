482e0c196aee2dc29642c0ed98c5ef53682206a2
Vinayak <vvinayaksaini2gmail.com>
2021-06-19 13:29:47 +0530

    adding colors.js file

---
+++ colors.js
@@ -0,0 +1,7 @@
+console.log("START colors.js ........................................")
+
+let selectedColors = ['red','black','blue'];
+console.log(selectedColors);
+console.log(selectedColors[1]);
+
+console.log("END of colors.js ...............................................");
--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+console.log("START index.js .......................................")
+
 let a =10;
 
 console.log(a);
+let person={
+    pName:'VS',
+    pAge:23
+};
+
+console.log(person);
+console.log("MY NAME IS =");
+console.log(person.pName);
+//Alternate method
+
+person['pName']='abc';
+console.log(person['pName']);
+
+let tmp ='pAge';
+console.log(person.pAge);
+person[tmp]=24;
+console.log(person[tmp]);
+
+
+console.log("END OF index.js ............................................");
